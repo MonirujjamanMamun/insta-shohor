1692c9e9867aaec04520debda8acf95682e27290
Monirujjaman Mamun <monirujjamanmamun2357@gmail.com>
2022-03-10 23:09:20 +0600

    all error are complited

--- js/app.js
+++ js/app.js
@@ -150,6 +150,7 @@ const showPosts = (posts) => {
 };
 
 const displayLikedPosts = () => {
+  document.getElementById("liked").textContent='';
     const likedPosts = getLikedPosts();
     likedPosts.forEach((post) => {
         const div = createPost(post);
@@ -158,12 +159,13 @@ const displayLikedPosts = () => {
 };
 
 const displayReportedPosts = () => {
+  document.getElementById("reported").textContent='';
     const reportedPosts = getReportedPosts();
     reportedPosts.forEach((post) => {
         const div = createPost(post);
         document.getElementById("reported").appendChild(div);
     });
-   
+    
 };
 
 const loadPosts = async() => {
